15a7ec8e73d8800eef177d9fcea82eb87f4abce6
alimazhabe-max <alimazhabe@gmail.com>
2026-07-31 13:02:00 +0330

    Update helpers.py

--- bot/utils/helpers.py
+++ bot/utils/helpers.py
@@ -104,7 +104,7 @@ def build_message(user_id, user_name, city):
         f"📌 **مناسبت‌های قمری فردا:**\n{hijri_tomorrow_text}\n\n" +
         f"📌 **مناسبت‌های شمسی امروز:**\n{shamsi_text}\n\n" +
         f"🔮 **مناسبت‌های شمسی فردا:**\n{shamsi_tomorrow_text}\n\n" +
-        get_text(user_id, "prayer", city=city) + "\n" + prayer_text + "\n" +
+        get_text(user_id, "prayer", city=city) + "\n" + prayer_text +
         next_prayer_text +
         get_text(user_id, "weather", city=city) + "\n" + weather_text + "\n\n" +
         get_text(user_id, "motivation") + "\n" + motivation + "\n\n" +
