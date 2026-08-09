a21530f28a633b3069e2214061479ab8d7aa4aa9
alimazhabe-max <alimazhabe@gmail.com>
2026-08-09 20:48:09 +0330

    Add files via upload

--- bot/features/tools/__init__.py
+++ bot/features/tools/__init__.py
@@ -1,1 +1,15 @@
+from .fun_tools import (
+    hafez_fal,
+    truth_or_dare,
+    joke_of_day,
+    fact_of_day,
+    daily_challenge,
+)
 
+__all__ = [
+    "hafez_fal",
+    "truth_or_dare",
+    "joke_of_day",
+    "fact_of_day",
+    "daily_challenge",
+]
