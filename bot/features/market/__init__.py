fc428e0ea5af6374cec61bbfd2991972f5047977
alimazhabe-max <alimazhabe@gmail.com>
2026-08-09 20:46:49 +0330

    Add files via upload

--- bot/features/market/__init__.py
+++ bot/features/market/__init__.py
@@ -1,1 +1,17 @@
+from .finance import (
+    full_market_prices,
+    convert_currency,
+    profit_loss,
+    parse_profit,
+    get_top_crypto,
+    convert_crypto,
+)
 
+__all__ = [
+    "full_market_prices",
+    "convert_currency",
+    "profit_loss",
+    "parse_profit",
+    "get_top_crypto",
+    "convert_crypto",
+]
