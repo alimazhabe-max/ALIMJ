7f589b9653159fa0c941fee482b54456e59e9dfc
alimazhabe-max <alimazhabe@gmail.com>
2026-08-10 20:37:33 +0330

    Update __init__.py

--- bot/features/tools/__init__.py
+++ bot/features/tools/__init__.py
@@ -1,9 +1,9 @@
 from .app_tools import (
-    convert_unit, parse_unit, calculator, generate_password, count_text,
+    calculator, generate_password, count_text,
     world_distance, geocode,
 )
 
 __all__ = [
-    "convert_unit", "parse_unit", "calculator", "generate_password", "count_text",
+    "calculator", "generate_password", "count_text",
     "world_distance", "geocode",
 ]
